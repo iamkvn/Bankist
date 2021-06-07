173d932b5becb78265368cb7927028b811e0e8f6
iamkevn <iamkevnrodriguez@gmail.com>
2021-06-07 19:19:18 -0400

    sticky nav on scroll

--- script.js
+++ script.js
@@ -125,3 +125,11 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+//STICKY NAVIGATION
+const initialCoords = section1.getBoundingClientRect();
+log(initialCoords);
+window.addEventListener('scroll', function () {
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
